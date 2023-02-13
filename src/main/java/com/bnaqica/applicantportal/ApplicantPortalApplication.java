235ccb686eb48deb37f3f13daa70f1910afff723
bnaqica <bnaqica@ysgco.com>
2023-02-13 07:44:31 -0700

    Initial commit of project sturcture

---
+++ src/main/java/com/bnaqica/applicantportal/ApplicantPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bnaqica.applicantportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantPortalApplication.class, args);
+	}
+
+}
